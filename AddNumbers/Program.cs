b0c60b535fabfd7d494214f5b71099f03469181f
Sophia Pastushenko <sofpastushenko@gmail.com>
2024-02-18 01:59:10 +0200

    Created C# Console App that sums two numbers

---
+++ AddNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Перше число: ");
+int num1;
+while (!int.TryParse(Console.ReadLine(), out num1))
+{
+    Console.Write("Введіть ціле число: ");
+}
+
+Console.Write("Друге число: ");
+int num2;
+while (!int.TryParse(Console.ReadLine(), out num2))
+{
+    Console.Write("Введіть ціле число: ");
+}
+
+Console.WriteLine("Сума чисел: " + (num1 + num2));
